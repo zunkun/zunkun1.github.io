0d53379fb68a7745ee3c8198865b73ebb80ff729
zunkun <liuzunkun@aliyun.com>
2021-01-05 22:47:36 +0800

    first commit

---
+++ themes/icarus/layout/common/footer.jsx
@@ -0,0 +1,65 @@
+const { Component } = require('inferno');
+const { cacheComponent } = require('hexo-component-inferno/lib/util/cache');
+
+class Footer extends Component {
+    render() {
+        const {
+            logo,
+            logoUrl,
+            siteUrl,
+            siteTitle,
+            siteYear,
+            author,
+            links,
+            showVisitorCounter,
+            visitorCounterTitle
+        } = this.props;
+
+        let footerLogo = '';
+        if (logo) {
+            if (logo.text) {
+                footerLogo = logo.text;
+            } else {
+                footerLogo = <img src={logoUrl} alt={siteTitle} height="28" />;
+            }
+        } else {
+            footerLogo = siteTitle;
+        }
+
+        return <footer class="footer">
+            <div class="container">
+                {footerLogo} {siteYear} &copy;
+                <a href="http://beian.miit.gov.cn/" target="_blank">豫ICP备20003453号-1</a>
+            </div>
+        </footer>;
+    }
+}
+
+module.exports = cacheComponent(Footer, 'common.footer', props => {
+    const { config, helper } = props;
+    const { url_for, _p, date } = helper;
+    const { logo, title, author, footer, plugins } = config;
+
+    const links = {};
+    if (footer && footer.links) {
+        Object.keys(footer.links).forEach(name => {
+            const link = footer.links[name];
+            links[name] = {
+                url: url_for(typeof link === 'string' ? link : link.url),
+                icon: link.icon
+            };
+        });
+    }
+
+    return {
+        logo,
+        logoUrl: url_for(logo),
+        siteUrl: url_for('/'),
+        siteTitle: title,
+        siteYear: date(new Date(), 'YYYY'),
+        author,
+        links,
+        showVisitorCounter: plugins && plugins.busuanzi === true,
+        visitorCounterTitle: _p('plugin.visitor_count', '<span id="busuanzi_value_site_uv">0</span>')
+    };
+});
